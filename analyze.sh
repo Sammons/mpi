b1e63eb074f4f19251fc5eb5a320dcd134f19fe0
Sammons <bsammnz@gmail.com>
2015-04-19 19:42:52 -0500

    more data collection

--- analyze.sh
+++ analyze.sh
@@ -20,11 +20,11 @@ echo "1) running 3 times per P, with a different vector each time "
 echo "   where P is the number of threads, and ranges from "
 echo "   1 to 16. we search for the 1500 nearest neighbors."
 N=1500;
-for i in `seq 1 16`; 
+for i in `seq 5 19`; # P (processes)
 do
 	echo $i
 	# perform run
-	for j in `seq 1 3`;
+	for j in `seq 1 10`; # vector seed
 	do
 		echo $j &&
 		mpirun -n $i src/mpi /cluster/content/hpc/distributed_data/ $j $N >& run_$i.txt &&
@@ -34,3 +34,58 @@ do
 		rm bds8c7_results.txt
 	done;
 done;
+
+# accumulate data and produce graphs
+# with N = 1500, 10 samples
+SAMPLE_COUNT=10;
+for i in `seq 5 19`; # P (processes)
+do
+	echo $i;
+	cat serial_search_$i.txt | awk '{s+=$1} END {print s}' | awk '{print $1/"'"$SAMPLE_COUNT"'"}' >> l_p_average_serial.txt;
+	cat parallel_search_$i.txt | awk '{s+=$1} END {print s}' | awk '{print $1/"'"$SAMPLE_COUNT"'"}' >> l_p_average_parallel.txt;
+done;
+paste l_p_average_serial.txt l_p_average_parallel.txt > l_p_averages_together.txt;
+cat l_p_averages_together.txt | awk '{ print $1/$2 }' > l_p_average_parallel_speedup.txt;
+
+echo "----------DONE-----------";
+# wall-clock & serial : vs P
+
+# ratio vs P
+
+echo "--------------------------------";
+echo "    P=8, N=100,500,1000,1500";
+echo "--------------------------------";
+
+
+# with P fixed at 8, 10 samples
+rm parallel*.txt;
+rm serial*.txt;
+P=8;
+for i in 100 500 1000 1500;
+do
+	echo $i
+	# perform run
+	for j in `seq 1 10`; # vector seed
+	do
+		echo $j &&
+		mpirun -n $P src/mpi /cluster/content/hpc/distributed_data/ $j $i >& run_$i.txt &&
+		cat run_$i.txt | grep -Eo ":[0-9].*$" | sed s/seconds// | grep -Eo "[0-9\.\e\-\+]*\b" | awk '{s+=$1} END {print s}' >> serial_search_$i.txt &&
+		cat bds8c7_results.txt | grep search-time | sed s/search-time:// | sed s/seconds// >> parallel_search_$i.txt &&
+		rm run_$i.txt &&
+		rm bds8c7_results.txt;
+	done;
+done;
+
+SAMPLE_COUNT=10;
+for i in 100 500 1000 1500;
+do
+	echo $i;
+	cat serial_search_$i.txt | awk '{s+=$1} END {print s}' | awk '{print $1/"'"$SAMPLE_COUNT"'"}' >> l_n_average_serial.txt;
+	cat parallel_search_$i.txt | awk '{s+=$1} END {print s}' | awk '{print $1/"'"$SAMPLE_COUNT"'"}' >> l_n_average_parallel.txt;
+done;
+paste l_n_average_serial.txt l_p_average_parallel.txt > l_n_averages_together.txt;
+cat l_n_averages_together.txt | awk '{ print $1/$2 }' > l_n_average_parallel_speedup.txt;
+
+echo "--------DONE---------"
+# wall-clock & serial : vs N
+# ratio vs N
